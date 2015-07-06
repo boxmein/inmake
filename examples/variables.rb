596747597fd5f98d1fcb85611567675ad626a340
boxmein <wsdf294@gmail.com>
2015-07-07 01:41:53 +0300

    add new variables into example

--- examples/variables.rb
+++ examples/variables.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/ruby
-# echo {{f}} {{dn}} {{bn}} {{AWESOME}} {{COOL}}
+# echo filename {{f}} dirname {{dn}} basename {{bn}} basename1 {{bn1}} AWESOME {{AWESOME}} COOL {{COOL}} extension {{ext}} mode {{mode}} inode-number {{inode}} mtime {{mtime}} ctime {{ctime}} size {{size}}
 # if all is well this should echo
 # current filename, dirname, basename, YES, NO
